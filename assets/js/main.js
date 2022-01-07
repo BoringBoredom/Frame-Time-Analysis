57ddb3c20b82b89dc8b0fb41410294b18b45c043
BoringBoredom <BoringBoredom>
2022-01-07 12:18:43 +0100

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,24 +21,30 @@ document.addEventListener('dragover', ev => {
     ev.stopPropagation()
     ev.preventDefault()
 })
-document.addEventListener('drop', ev => {
+document.addEventListener('drop', async ev => {
     ev.stopPropagation()
     ev.preventDefault()
     for (const file of ev.dataTransfer.files) {
-        if (!file.name.endsWith('.csv')) {
-            continue
+        const fileName = file.name
+        if (fileName.endsWith('.csv')) {
+            instructions.style.display = 'none'
+            Papa.parse(file, {
+                complete: (result) => {
+                    processCSV(fileName, ++fileCount, result.data)
+                }
+            })
         }
-        instructions.style.display = 'none'
-        Papa.parse(file, {
-            complete: (result) => appendBench(file.name, result.data, ++fileCount)
-        })
+        else if (fileName.endsWith('.json')) {
+            instructions.style.display = 'none'
+            processJSON(fileName, ++fileCount, JSON.parse(await file.text()))
+        }
     }
 })
 
 const comparisonChart = new Chart(comparison, {
     type: 'bar',
     data: {
-        labels: benches.map(bench => bench.fileName),
+        labels: benches.map(bench => bench.file_name),
         datasets: [{
             label: metric.value,
             data: benches.map(bench => bench[metric.value]),
@@ -69,45 +75,19 @@ const comparisonChart = new Chart(comparison, {
     plugins: [ChartDataLabels]
 })
 
-function appendBench(fileName, data, fileCount) {
-    let infoRow
-    for (const [index, row] of data.entries()) {
-        if (row.includes('MsBetweenPresents')) {
-            infoRow = row
-            data = data.slice(index + 1)
-            break
-        }
-    }
+function completeStats(bench) {
+    const frameTimes = bench.frame_times
+    const benchmarkTime = bench.benchmark_time * 1000
+    const frameCount = bench.frame_count
 
-    const firstRow = data[0]
+    const sortedFrameTimes = [...frameTimes].sort((a, b) => b - a)
+    bench.sorted_frame_times = sortedFrameTimes
 
-    const bench = {
-        fileName: fileName,
-        application: firstRow[infoRow.indexOf('Application')],
-        present_mode: firstRow[infoRow.indexOf('PresentMode')]
-    }
-
-    const frametimes = []
-    const horGraphAxis = []
-    const values = [1, 0.1, 0.01]
-    const index = infoRow.indexOf('MsBetweenPresents')
-
-    let frameCount = 0
-    let benchmarkTime = 0
-    for (const row of data) {
-        const present = parseFloat(row[index])
-        if (present) {
-            frametimes.push(present)
-            frameCount++
-            benchmarkTime += present
-            horGraphAxis.push(benchmarkTime)
-        }
-    }
-
-    const sortedFrameTimes = [...frametimes].sort((a, b) => b - a)
     bench['Max'] = (1000 / sortedFrameTimes.at(-1)).toFixed(2)
     bench['Avg'] = (1000 / (benchmarkTime / frameCount)).toFixed(2)
     bench['Min'] = (1000 / sortedFrameTimes[0]).toFixed(2)
+
+    const values = [1, 0.1, 0.01]
 
     for (const percentile of values) {
         const fps = 1000 / sortedFrameTimes[Math.ceil(percentile / 100 * frameCount) - 1]
@@ -127,10 +107,16 @@ function appendBench(fileName, data, fileCount) {
         }
     }
 
+    appendBench(bench)
+}
+
+function appendBench(bench) {
+    const fileCount = bench.file_count
+
     benchmarks.insertAdjacentHTML('beforeend', `
         <div class="bench">
             <p>
-                ${fileName} | ${bench.application} | ${bench.present_mode}
+                ${bench.file_name} | ${bench.application} | ${bench.runtime} | ${bench.present_mode}
             </p>
             <div class="charts">
                 <div class="column">
@@ -182,10 +168,10 @@ function appendBench(fileName, data, fileCount) {
     const scatter = new Chart(document.getElementById(`scatter-${fileCount}`), {
         type: 'scatter',
         data: {
-            labels: horGraphAxis,
+            labels: bench.elapsed,
             datasets: [{
                 label: 'Frame Times',
-                data: frametimes,
+                data: bench.frame_times,
                 backgroundColor: 'rgb(0,191,255)',
                 borderWidth: 0,
                 radius: 1
@@ -207,6 +193,81 @@ function appendBench(fileName, data, fileCount) {
     results.removeAttribute('style')
 
     updateComparison()
+}
+
+function processCSV(fileName, fileCount, data) {
+    let infoRow
+    for (const [index, row] of data.entries()) {
+        if (row.includes('MsBetweenPresents')) {
+            infoRow = row
+            data = data.slice(index + 1)
+            break
+        }
+    }
+
+    const firstRow = data[0]
+
+    const bench = {
+        file_name: fileName,
+        file_count: fileCount,
+        application: firstRow[infoRow.indexOf('Application')],
+        present_mode: firstRow[infoRow.indexOf('PresentMode')],
+        runtime: firstRow[infoRow.indexOf('Runtime')]
+    }
+
+    const frametimes = []
+    const elapsed = []
+
+    const presentIndex = infoRow.indexOf('MsBetweenPresents')
+    const elapsedIndex = infoRow.indexOf('TimeInSeconds')
+
+    let frameCount = 0
+    for (const row of data) {
+        const present = parseFloat(row[presentIndex])
+        const currentElapsed = parseFloat(row[elapsedIndex])
+        if (present && currentElapsed) {
+            frametimes.push(present)
+            elapsed.push(currentElapsed)
+            frameCount++
+        }
+    }
+
+    bench.frame_times = frametimes
+    bench.elapsed = elapsed
+    bench.frame_count = frameCount
+    bench.benchmark_time = elapsed.at(-1)
+
+    completeStats(bench)
+}
+
+const cfxPresentModes = {
+    1: 'Hardware: Legacy Flip',
+    2: 'Hardware: Legacy Copy to front buffer',
+    3: 'Hardware: Independent Flip',
+    4: 'Composed: Flip',
+    5: 'Hardware Composed: Independent Flip',
+    6: 'Composed: Copy with GPU GDI',
+    7: 'Composed: Copy with CPU GDI',
+    8: 'Composed: Composition Atlas'
+}
+
+function processJSON(fileName, fileCount, data) {
+    const run = data['Runs'][0]
+
+    const bench = {
+        file_name: fileName,
+        file_count: fileCount,
+        application: data['Info']['ProcessName'],
+        present_mode: cfxPresentModes[run['CaptureData']['PresentMode']?.[0]],
+        runtime: run['PresentMonRuntime']
+    }
+
+    bench.frame_times = run['CaptureData']['MsBetweenPresents']
+    bench.elapsed = run['CaptureData']['TimeInSeconds']
+    bench.frame_count = bench.frame_times.length
+    bench.benchmark_time = bench.elapsed.at(-1)
+
+    completeStats(bench)
 }
 
 exportButton.addEventListener('click', ev => {
@@ -235,7 +296,7 @@ metric.addEventListener('click', ev => {
 })
 
 function updateComparison() {
-    comparisonChart.data.labels = benches.map(bench => bench.fileName)
+    comparisonChart.data.labels = benches.map(bench => bench.file_name)
     comparisonChart.data.datasets[0].label = metric.value
     comparisonChart.data.datasets[0].data = benches.map(bench => bench[metric.value])
     comparisonChart.update()
