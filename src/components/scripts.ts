BoringBoredom/Frame-Time-Analysis

commit 27ddda97a14175dd966e359b6563458c25430c35

adapt to more upcoming PM changes

src/components/scripts.ts
@@ -216,9 +216,11 @@ function processCsv(
       presentModes.add(splitLine[presentModeIndex]);
       syncIntervals.add(splitLine[syncIntervalIndex]);
 
+      const displayedTime = parseFloat(splitLine[displayedTimeIndex]);
       if (
         parseInt(splitLine[droppedIndex], 10) === 1 ||
-        parseFloat(splitLine[displayedTimeIndex]) === 0
+        displayedTime === 0 ||
+        Number.isNaN(displayedTime)
       ) {
         dropped += 1;
       }

commit 26760200dae8f72f6899b0b22adff948bb55d441

fix dropped frame parsing 27ddda97a14175dd966e359b6563458c25430c35 broke

src/components/scripts.ts
@@ -219,8 +219,8 @@ function processCsv(
       const displayedTime = parseFloat(splitLine[displayedTimeIndex]);
       if (
         parseInt(splitLine[droppedIndex], 10) === 1 ||
-        displayedTime === 0 ||
-        Number.isNaN(displayedTime)
+        (displayedTimeIndex !== -1 &&
+          (displayedTime === 0 || Number.isNaN(displayedTime)))
       ) {
         dropped += 1;
       }